1e2663fe8837171970e267377009e79b9aa64381
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-10-07 12:43:32 -0700

    Working on caching setup for /products route

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const db = require('./database/');
+const nodeCache = require('node-cache');
 const { Pool } = require('pg');
 
 const app = express();
 app.use(express.json());
 
+const cache = new nodeCache({ maxKeys: 100 });
 
-app.get('/products', (req, res) => {
 
-  db.getProducts([req.query.page || 1, req.query.count || 5])
-    .then((result) => res.end(JSON.stringify((result.rows))));
+app.get('/products', async (req, res) => {
+  const { page = 5, count = 1 } = req.query;
+  let cacheKey = `/products?p=${page},c=${count}`;
+
+  if (cache.has(cacheKey)) {
+    res.status(200).send(cache.get(cacheKey));
+  } else {
+    db.getProducts([page || 1, count || 5])
+    .then(products=>console.log(products.rows));
+    cache.set(cacheKey, products);
+    res.end(JSON.stringify((products.rows)));
+  }
 });
 
 app.get('/products/:product_id', (req, res) => {
@@ -28,7 +39,7 @@ app.get('/products/:product_id/styles', (req, res) => {
 app.get('/products/:product_id/related', (req, res) => {
 
   db.getRelated([req.params.product_id])
-    .then((results)=>res.end(JSON.stringify(results.rows[0].array_agg)))
+    .then((results) => res.end(JSON.stringify(results.rows[0].array_agg)))
     .catch((err) => { console.error(err); res.sendStatus(404); });
 });
 
